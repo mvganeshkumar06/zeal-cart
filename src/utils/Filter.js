8c108aa9d53c2c0f5f0e9f377e652458c0474de3
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-29 15:27:15 +0530

    refactor : updated utils

--- src/utils/Filter.js
+++ src/utils/Filter.js
@@ -2,4 +2,12 @@ const filterByRange = (arr, range) => {
     return arr.filter((product) => parseInt(product.price) <= parseInt(range));
 };
 
-export default filterByRange;
+const filterByCategory = (arr, category) => {
+    return arr.filter(
+        (product) =>
+            product.category.name.toLowerCase() ===
+            category.replace("_", " ").toLowerCase()
+    );
+};
+
+export { filterByRange, filterByCategory };
