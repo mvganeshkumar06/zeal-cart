b654305b5c09317010dea4cb4ac4dbe0695c3866
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-06-19 15:43:52 +0530

    refactor : updated component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import { Container, useStyleContext, useThemeContext, Image } from "@zeal-ui/core";
+import { Container, useStyleContext, useThemeContext, Text, InlineCode } from "@zeal-ui/core";
 
 const Footer = () => {
     const style = useStyleContext();
@@ -17,43 +17,61 @@ const Footer = () => {
         position: relative;
         bottom: 0;
         
-        .icon{
-            margin:0rem 0.5rem;
+        .text{
+            font-size:0.95rem;
+            display:flex;
+            flex-direction:row;
+            align-items:center;
+            margin:0rem 0.25rem;
         }
 
-        .icon:hover{
+        .inlineCode{
+            font-size:0.85rem;
+            padding:0rem 0.5rem;
+        }
+
+        .icon{
+            width:1.25rem;
+            height:1.25rem;
+            margin:0rem 0.25rem;
+        }
+
+        .icon:hover, .zealUI:hover{
             cursor:pointer;
         }
     `;
 
     return (
         <Container type="row" rowCenter colCenter height="3rem" customStyles={styles}>
-            Made with <FavoriteIcon className="icon" /> by Ganesh Kumar.
-            Powered by
             <a href="https://zeal-ui.netlify.app" target="_blank" rel="noreferrer">
-                <Image src="https://zeal-cart.netlify.app/assets/zeal-ui-icon.svg" alt="Zeal UI Icon" className="icon" />
-            </a>.
-            <a
-                href="https://www.linkedin.com/in/mvganeshkumar06/"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <LinkedInIcon className="icon" />
+                <Text className="text zealUI">Powered by
+                    <InlineCode color="blue" className="inlineCode">Zeal UI</InlineCode>
+                </Text>
             </a>
-            <a
-                href="https://github.com/mvganeshkumar06"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <GitHubIcon className="icon" />
-            </a>
-            <a
-                href="https://twitter.com/mvganeshkumar06"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <TwitterIcon className="icon" />
-            </a>
+            <Container type="row" colCenter>
+                <Text className="text">Made with <FavoriteIcon className="icon" /> by <InlineCode color="blue" className="inlineCode">Ganesh Kumar </InlineCode></Text>
+                <a
+                    href="https://www.linkedin.com/in/mvganeshkumar06/"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <LinkedInIcon className="icon" />
+                </a>
+                <a
+                    href="https://github.com/mvganeshkumar06"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <GitHubIcon className="icon" />
+                </a>
+                <a
+                    href="https://twitter.com/mvganeshkumar06"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <TwitterIcon className="icon" />
+                </a>
+            </Container>
         </Container>
     );
 };
