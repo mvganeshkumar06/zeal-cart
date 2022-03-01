0982c865081b34fa39a2a07a7aac5d959418c1b1
M V Ganesh Kumar <contact@mvganeshkumar.com>
2022-03-01 17:15:30 +0530

    updated router

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-import { Route, Redirect } from "react-router";
+import React from 'react';
+import { Route, Redirect } from 'react-router';
 
 const PrivateRoute = ({ path, children, ...rest }) => {
-
-    const savedAccessToken = localStorage.getItem("user-access-token");
-
-    return (
-        <Route path={path} {...rest}>
-            {savedAccessToken ? children : <Redirect to={{ pathname: "/login", state: { pathAfterLogin: path } }} />}
-        </Route>
-    );
+	const savedAccessToken = localStorage.getItem('user-access-token');
+	return (
+		<Route path={path} {...rest}>
+			{savedAccessToken ? (
+				children
+			) : (
+				<Redirect to={{ pathname: '/login', state: { pathAfterLogin: path } }} />
+			)}
+		</Route>
+	);
 };
 
 export default PrivateRoute;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,40 +1,42 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Header, Navigation, Footer } from "../components";
-import { Home, Products, WishList, Cart, Auth, Product, NotFound } from "../pages";
-import PrivateRoute from "./PrivateRoute";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Layout } from '@zeal-ui/core';
+import { Header, Sidebar } from '../components';
+import { Home, Products, WishList, Cart, Auth, Product, NotFound } from '../pages';
+import PrivateRoute from './PrivateRoute';
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Navigation />
-            <Switch>
-                <Route path="/products/:productId">
-                    <Product />
-                </Route>
-                <Route path="/products">
-                    <Products />
-                </Route>
-                <PrivateRoute path="/wishlist">
-                    <WishList />
-                </PrivateRoute>
-                <PrivateRoute path="/cart">
-                    <Cart />
-                </PrivateRoute>
-                <Route path="/login">
-                    <Auth />
-                </Route>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="*">
-                    <NotFound />
-                </Route>
-            </Switch>
-            <Footer />
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Header />
+			<Sidebar />
+			<Layout>
+				<Switch>
+					<Route path="/products/:productId">
+						<Product />
+					</Route>
+					<Route path="/products">
+						<Products />
+					</Route>
+					<PrivateRoute path="/wishlist">
+						<WishList />
+					</PrivateRoute>
+					<PrivateRoute path="/cart">
+						<Cart />
+					</PrivateRoute>
+					<Route path="/login">
+						<Auth />
+					</Route>
+					<Route path="/" exact>
+						<Home />
+					</Route>
+					<Route path="*">
+						<NotFound />
+					</Route>
+				</Switch>
+			</Layout>
+		</BrowserRouter>
+	);
 };
 
 export default Router;
